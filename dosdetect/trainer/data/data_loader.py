12ae2fce838872274fd0f6c9bfdfd6f11b58afea
Sean Sica <23294618+seansica@users.noreply.github.com>
2024-04-07 16:31:06 -0400

    Update data_loader.py to load a fraction of data while maintaining class distribution

--- dosdetect/trainer/data/data_loader.py
+++ dosdetect/trainer/data/data_loader.py
@@ -22,26 +22,51 @@ class DataLoader:
         self.file_paths = file_paths
         logger.debug(f"DataLoader initialized with file paths: {file_paths}")
 
-    def load_data(self):
+    def load_data(self, fraction=1.0):
         """
-        Load data from CSV files and concatenate them into a single DataFrame.
+        Load a fraction of data from CSV files while maintaining class distribution.
+
+        Args:
+            fraction (float): Fraction of data to load (default: 1.0).
 
         Returns:
-            pandas.DataFrame: Concatenated DataFrame containing all the loaded data.
+            pandas.DataFrame: Loaded data as a DataFrame.
         """
-        logger.info("Loading data from CSV files...")
+        logger.info(f"Loading {fraction*100}% of data from CSV files...")
         data_frames = []
 
         # Iterate over each file path
         for file_path in self.file_paths:
             # Read CSV file into a DataFrame
             df = pd.read_csv(file_path)
-            data_frames.append(df)
             logger.debug(f"Loaded data from {file_path}. Shape: {df.shape}")
 
-        # Concatenate all DataFrames into a single DataFrame
+            # Get the unique labels and their counts
+            label_counts = df[" Label"].value_counts()
+            labels = label_counts.index
+            counts = label_counts.values
+
+            # Calculate the number of samples to load for each label
+            num_samples = (counts * fraction).astype(int)
+
+            # Iterate over each label and load the corresponding samples
+            loaded_data = []
+            for label, count, num in zip(labels, counts, num_samples):
+                if num > 0:
+                    loaded_data.append(df[df[" Label"] == label][:num])
+
+            # Concatenate the loaded data for the current file
+            loaded_df = pd.concat(loaded_data, ignore_index=True)
+            data_frames.append(loaded_df)
+            logger.debug(
+                f"Loaded {fraction*100}% of data from {file_path}. Shape: {loaded_df.shape}"
+            )
+
+        # Concatenate all loaded DataFrames into a single DataFrame
         all_data = pd.concat(data_frames, ignore_index=True)
-        logger.info(f"All data loaded and concatenated. Shape: {all_data.shape}")
+        logger.info(
+            f"{fraction*100}% of data loaded and concatenated. Shape: {all_data.shape}"
+        )
 
         return all_data
 
